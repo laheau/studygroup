1844c25260d4c73eed427aab0f905cdc9c036eeb
AMNbot <aymenouali01@gmail.com>
2022-11-20 01:10:09 -0500

    Quizz

--- client/codejam12/pages/quizz/[id].js
+++ client/codejam12/pages/quizz/[id].js
@@ -1,75 +1,116 @@
-import { useRouter } from "next/router";
-import { useState, useEffect } from "react";
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import { color } from "@mui/system";
-import { CenterFocusStrong } from "@mui/icons-material";
-import TextField from "@mui/material/TextField";
+import React, { useState, useMemo, useRef } from 'react'
+import TinderCard from 'react-tinder-card'
 
+const db = [
+    {
+        question: 'What is the difference between inheritance and polymorphism',
+        answer: 'Idk look it up'
+    },
+    {
+        question: 'What is encapsulation in Java',
+        answer: 'Idk look it up'
+    },
+    {
+        question: 'What is the time complexity of inserting inside a LinkedList',
+        answer: 'Idk look it up'
+    },
+    {
+        question: 'Python or Java?',
+        answer: 'VHDL is king'
+    },
+    {
+        question: 'Sweet or Savoury?',
+        answer: 'Sweet (I am a manchild)'
+    }
+]
 
-const card = {
-    "question": "Hi how are you?",
-    "answer": "Great!"
-}
+function Quizz() {
+    const [currentIndex, setCurrentIndex] = useState(db.length - 1)
+    const [lastDirection, setLastDirection] = useState()
+    // used for outOfFrame closure
+    const currentIndexRef = useRef(currentIndex)
 
-const Quizz = () => {
-    const router = useRouter();
-    const { id } = router.query;
+    const childRefs = useMemo(
+        () =>
+            Array(db.length)
+                .fill(0)
+                .map((i) => React.createRef()),
+        []
+    )
 
-    const [filter, setFilter] = useState("");
-    const [cards, setCards] = useState([]);
-    const [fetched, setFetched] = useState(false);
-    const [showAnswer, setShowAnswer] = useState(false);
-    const [fetchedCards, setFetchedCards] = useState(false);
-    const [filteredCards, setFilteredCards] = useState([]);
+    const updateCurrentIndex = (val) => {
+        setCurrentIndex(val)
+        currentIndexRef.current = val
+    }
 
-    useEffect(() => {
-        if (!fetched) {
-            console.log(id);
-            fetch(`http://localhost:8000/student/${id}/cards`)
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.result != undefined) {
-                        setCards(data.result);
-                        setFetched(true);
-                    }
-                });
+    const canGoBack = currentIndex < db.length - 1
+
+    const canSwipe = currentIndex >= 0
+
+    // set last direction and decrease current index
+    const swiped = (direction, nameToDelete, index) => {
+        setLastDirection(direction)
+        updateCurrentIndex(index - 1)
+    }
+
+    const outOfFrame = (name, idx) => {
+        console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current)
+        // handle the case in which go back is pressed before card goes outOfFrame
+        currentIndexRef.current >= idx && childRefs[idx].current.restoreCard()
+        // TODO: when quickly swipe and restore multiple times the same card,
+        // it happens multiple outOfFrame events are queued and the card disappear
+        // during latest swipes. Only the last outOfFrame event should be considered valid
+    }
+
+    const swipe = async (dir) => {
+        if (canSwipe && currentIndex < db.length) {
+            await childRefs[currentIndex].current.swipe(dir) // Swipe the card!
         }
-    }, []);
+    }
+
+    // increase current index and show card
+    const goBack = async () => {
+        if (!canGoBack) return
+        const newIndex = currentIndex + 1
+        updateCurrentIndex(newIndex)
+        await childRefs[newIndex].current.restoreCard()
+    }
 
     return (
-        <div>
-            <div className="flex flex-col w-fit m-auto items-center h-screen content-center">
-
-                <div className="w-[500px] h rounded-lg p-[15px] my-2">
-                    <Accordion style={{ borderRadius: 15, backgroundColor: "transparent", color: 'white', fontSize: 20 }}>
-                        <AccordionSummary
-                            expandIcon={<ExpandMoreIcon />}
-                            aria-controls="panel1a-content"
-                            id="panel1a-header"
-                            style={{ height: '300px', backgroundColor: '#66b2ff', borderRadius: 15, flexDirection: "column" }}
+        <div className='mx-auto h-screen w-screen flex justify-center items-center relative'>
+            <div className='relative'>
+                <div className='relative mt-4 mx-4 h-full'>
+                    {db.map((character, index) => (
+                        <TinderCard
+                            ref={childRefs[index]}
+                            className='swipe'
+                            key={character.question}
+                            onSwipe={(dir) => swiped(dir, character.question, index)}
+                            onCardLeftScreen={() => outOfFrame(character.question, index)}
                         >
-                            <div className="flex flex-col items-center">
-
-                                {card.tags ? <Typography fontWeight={16} textAlign='right'  >{card.tags}</Typography> : null}
-                                <Typography fontWeight={"bold"} fontSize={20} textAlign={'center'} >{card.question}</Typography>
+                            <div className='absolute -bottom-4 scale-[0.85] origin-bottom inset-x-0 h-[300px] w-[600px] mx-[-240px] backdrop-blur-md rounded-2xl bg-[#66b2ff] align-middle text-center'>
+                                <h3 className='text-white text-2xl my-[20%]'>{character.question}</h3>
                             </div>
-                        </AccordionSummary>
-                        <AccordionDetails style={{ height: '300px', backgroundColor: "#204f85", borderRadius: 15, justifyContent: "center" }}>
-                            <Typography fontWeight={"semi-bold"} fontSize={20} textAlign={'center'} >
-                                {card.answer}
-                            </Typography>
-                        </AccordionDetails>
-                    </Accordion>
+                        </TinderCard>
+                    ))}
                 </div>
-
             </div>
+            <div className='relative top-[15%] text-white space-x-10'>
+                {/* <button className='bg-[#ff8566] p-[20px] rounded-lg' onClick={() => swipe('left')}>Wrong!</button> */}
+                <button className='p-[20px] rounded-lg bg-yellow-300' onClick={() => goBack()}>Go back</button>
+                {/* <button className='bg-[#5aff91] p-[20px] rounded-lg' onClick={() => swipe('right')}>Correct!</button> */}
+            </div>
+            {/* {lastDirection ? (
+                <h2 key={lastDirection} className='infoText'>
+                    {lastDirection}
+                </h2>
+            ) : (
+                <h2 className='infoText'>
+                    Swipe a card or press a button to get Restore Card button visible!
+                </h2>
+            )} */}
         </div>
-
-    );
+    )
 }
 
-export default Quizz;
+export default Quizz
